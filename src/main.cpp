fa1efaaee6ebdab081928f085ebe3b18fe7a4f13
Nick Fox-Gieg <nick@fox-gieg.com>
2020-10-23 14:56:08 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,28 @@
-// =============================================================================
-//
-// Copyright (c) 2013 Christopher Baker <http://christopherbaker.net>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-// =============================================================================
-
-
+#include "ofMain.h"
 #include "ofApp.h"
 
+//========================================================================
+int main() {
+	
+	int w = 640;
+	int h = 480;
+	
+	// setup the GL context
+#ifdef TARGET_OPENGLES
+	ofGLESWindowSettings settings;
+	settings.glesVersion = 2;
+	settings.setSize(w, h);
+	ofCreateWindow(settings);
+#else
+        ofGLFWWindowSettings settings;
+        settings.numSamples = 0;
+	settings.setSize(w, h);
+        ofCreateWindow(settings);                       
+#endif
 
-int main()
-{
-	ofSetupOpenGL(320, 240, OF_WINDOW);
-	ofRunApp(new ofApp());
+        // this kicks off the running of my app
+        // can be OF_WINDOW or OF_FULLSCREEN
+        // pass in width and height too:
+        ofRunApp( new ofApp());
+
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,54 +1,117 @@
-// =============================================================================
-//
-// Copyright (c) 2013 Christopher Baker <http://christopherbaker.net>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-// =============================================================================
-
-
 #pragma once
 
-
 #include "ofMain.h"
-#include "ofxJSONElement.h"
+#include "ofxCv.h"
+#include "ofxCvPiCam.h"
+#include "ofxOsc.h"
+#include "ofxXmlSettings.h"
 #include "ofxHTTP.h"
+#include "ofxJSONElement.h"
+#include "ofxCrypto.h"
 
+#define NUM_MESSAGES 30 // how many past ws messages we want to keep
 
-using namespace ofx;
+class ofApp : public ofBaseApp {
 
+    public:	
+	void setup();
+	void update();
+	void draw();
+		
+	int width, height, appFramerate, camFramerate;
+	
+	string uniqueId, hostName; 
+	string oscHost;
+	int oscPort, streamPort, wsPort, postPort;
 
-class ofApp: public ofBaseApp
-{
-public:
-    void setup();
-    void update();
-    void draw();
+	bool debug; // draw to local screen, default true
 
-    void onWebSocketOpenEvent(HTTP::WebSocketEventArgs& evt);
-    void onWebSocketCloseEvent(HTTP::WebSocketEventArgs& evt);
-    void onWebSocketFrameReceivedEvent(HTTP::WebSocketFrameEventArgs& evt);
-    void onWebSocketFrameSentEvent(HTTP::WebSocketFrameEventArgs& evt);
-    void onWebSocketErrorEvent(HTTP::WebSocketEventArgs& evt);
+	ofFile file;
+	ofxXmlSettings settings;
 
-    ofColor bgColor;
+	ofFbo fbo;
+	ofPixels pixels;
+	int rpiCamVersion; // 0 for not an RPi cam, 1, 2, or 3
+	string lastPhotoTakenName;
+	int stillCompression;
+	int timestamp;
+	
+	void createResultHtml(string fileName);
+	void takePhoto();
 
-    HTTP::BasicWebSocketServer::SharedPtr server;
+	ofxHTTP::SimpleIPVideoServer streamServer;
+	ofxHTTP::SimpleIPVideoServerSettings streamSettings;
+	vector<string> photoFiles;
+
+	ofxHTTP::SimplePostServer postServer;
+	ofxHTTP::SimplePostServerSettings postSettings;
+	void onHTTPPostEvent(ofxHTTP::PostEventArgs& evt);
+	void onHTTPFormEvent(ofxHTTP::PostFormEventArgs& evt);
+	void onHTTPUploadEvent(ofxHTTP::PostUploadEventArgs& evt);
+	    	       
+    ofxHTTP::SimpleWebSocketServer wsServer;  
+	ofxHTTP::SimpleWebSocketServerSettings wsSettings;
+	void onWebSocketOpenEvent(ofxHTTP::WebSocketEventArgs& evt);
+	void onWebSocketCloseEvent(ofxHTTP::WebSocketCloseEventArgs& evt);
+	void onWebSocketFrameReceivedEvent(ofxHTTP::WebSocketFrameEventArgs& evt);
+	void onWebSocketFrameSentEvent(ofxHTTP::WebSocketFrameEventArgs& evt);
+	void onWebSocketErrorEvent(ofxHTTP::WebSocketErrorEventArgs& evt);
+    
+	// ~ ~ ~ ~ ~ ~ ~     
+
+	bool sendOsc;  // send osc, default true
+	bool sendWs;  // send websockets, default true
+	bool sendHttp;  // serve web control panel, default true
+	bool sendMjpeg;  // send mjpeg stream, default true	
+	bool syncVideo;  // send video image over osc, default false
+	bool brightestPixel;  // send brightest pixel, default false
+	bool blobs;  // send blob tracking, default true
+	bool contours; // send contours, default false
+
+	ofBuffer videoBuffer;
+	ofBuffer photoBuffer;
+	ofBuffer contourColorBuffer;
+	ofBuffer contourPointsBuffer;
+
+	ofxCvPiCam cam;
+	cv::Mat frame, frameProcessed;
+	ofImage gray;
+	int syncVideoQuality; // 5 best to 1 worst, default 3 medium
+	bool videoColor;
+
+	// for more camera settings, see:
+	// https://github.com/orgicus/ofxCvPiCam/blob/master/example-ofxCvPiCam-allSettings/src/testApp.cpp
+
+    int camShutterSpeed; // 0 to 330000 in microseconds, default 0
+    int camSharpness; // -100 to 100, default 0
+    int camContrast; // -100 to 100, default 0
+    int camBrightness; // 0 to 100, default 50
+	int camIso; // 100 to 800, default 300
+	int camExposureCompensation; // -10 to 10, default 0;
+
+	// 0 off, 1 auto, 2 night, 3 night preview, 4 backlight, 5 spotlight, 6 sports, 7, snow, 8 beach, 9 very long, 10 fixed fps, 11 antishake, 12 fireworks, 13 max
+	int camExposureMode; // 0 to 13, default 0
+
+	//string oscAddress;
+	int thresholdValue; // default 127
+	int thresholdKeyCounter;
+	bool thresholdKeyFast;
+
+	ofxOscSender sender;
+	void sendOscVideo();
+	void sendOscBlobs(int index, float x, float y);
+	void sendOscContours(int index);
+	void sendOscPixel(float x, float y);
+	void sendWsVideo();
+	void sendWsBlobs(int index, float x, float y);
+	void sendWsContours(int index);
+	void sendWsPixel(float x, float y);
+
+	ofxCv::ContourFinder contourFinder;
+	float contourThreshold;  // default 127
+	float contourMinAreaRadius; // default 10
+	float contourMaxAreaRadius; // default 150
+	int contourSlices; // default 20
+	ofxCv::TrackingColorMode trackingColorMode; // RGB, HSV, H, HS; default RGB
 
 };
